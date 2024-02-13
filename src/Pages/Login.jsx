e45a5d8100c58f7dbb8d70901ea26c9eb4ffdc8d
Azeem Shaik <azeemshaik025@gmail.com>
2024-02-13 17:14:32 +0530

    Packages Install

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -3,24 +3,23 @@ import "./Login.css";
 import axios from 'axios';
 import { Bounce, toast } from 'react-toastify';
 export default function Login() {
-    const [formData,setFormData] = useState({
-        username : '',
-        password : ''
+    const [formData, setFormData] = useState({
+        username: '',
+        password: ''
     });
-    const handleFormChange = (e)=>{
+    const handleFormChange = (e) => {
         setFormData({
-            ...formData,[e.target.name]:e.target.value
+            ...formData, [e.target.name]: e.target.value
         });
     }
-    const handleFormSubmit = async()=>{
+    const handleFormSubmit = async () => {
         try {
             const response = (await axios.post("http://localhost:3001/api/user/login/",
-            {
-                regNo : formData.username,
-                password : formData.password
-            }).data);
-            if(response.status===false)
-            {
+                {
+                    regNo: formData.username,
+                    password: formData.password
+                }).data);
+            if (response.status === false) {
                 toast.error("Login failed!", {
                     position: "top-right",
                     autoClose: 5000,
@@ -30,7 +29,7 @@ export default function Login() {
                     draggable: true,
                     progress: undefined,
                     theme: "dark",
-                    });
+                });
             }
         } catch (error) {
             toast.error("Login failed!", {
@@ -42,19 +41,19 @@ export default function Login() {
                 draggable: true,
                 progress: undefined,
                 theme: "dark",
-                });
+            });
         }
     }
-  return (
-    <div className='login-page'>
-      <div className='login-controller'>
-        <div>
-            <h1>Login</h1>
-            <input className='login-inputs' name='username' type='text' placeholder='Enter RegdNo' value={formData.username} onChange={handleFormChange}/>
-            <input className='login-inputs' value={formData.password} name='password' type='password' onChange={handleFormChange} placeholder='Enter password'/>
-            <button onClick={handleFormSubmit} type='submit' className='login-btn'>Login</button>
+    return (
+        <div className='login-page'>
+            <div className='login-controller'>
+                <div>
+                    <h1>Login</h1>
+                    <input className='login-inputs' name='username' type='text' placeholder='Enter RegdNo' value={formData.username} onChange={handleFormChange} />
+                    <input className='login-inputs' value={formData.password} name='password' type='password' onChange={handleFormChange} placeholder='Enter password' />
+                    <button onClick={handleFormSubmit} type='submit' className='login-btn'>Login</button>
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  )
+    )
 }
